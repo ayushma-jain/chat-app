2d6229d288b1d134a0d3daf3fddf516ec0ea917b
ayushma-jain <ayushmaj46@gmail.com>
2022-10-06 12:47:08 +0530

    Made some changes

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import { FiSend } from 'react-icons/fi';
+import Button from "react-bootstrap/Button";
 
+const chatData = ['Hi', 'Hello', 'How are you?', 'Good! Thank you for asking'];
 const ChatPage = () => {
     return (
-        <div className='i-box'>
+        <div className='i-box p-rel'>
             <h3 className='text-success'>Chat Page </h3>
+            <div className='message-box'>
+                {chatData.map(row => (
+                    <p className='text-bright alert-success'>{row}</p>
+                ))}
+            </div>
+            <div className='chat-box col-12'>
+                <div className='row'>
+                    <Form.Control type="text" className='col-md-10'
+                        placeholder="Type start.....">
+                    </Form.Control>
+                    <Button className='col-md-2 btn-success'><FiSend /></Button>
+                </div>
+            </div>
         </div>
     )
 }
